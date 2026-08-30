cfa448c129178111718182752ca0dbc110eab17d
Safiyah Lakhany <salakhany@gmail.com>
2026-08-29 23:48:44 -0700

    Connect admin page to publishing Worker

--- admin/config.js
+++ admin/config.js
@@ -3,5 +3,5 @@
 window.SARF_ADMIN_CONFIG = {
   workerUrl: ["localhost", "127.0.0.1"].includes(window.location.hostname)
     ? "http://localhost:8787/"
-    : ""
+    : "https://sarf-resource-publisher.salakhany.workers.dev/"
 };
